8ad282d06643d499bc3393b1efdbe114b05bf9f1
Shivam <shiwamsingh50@gmail.com>
2025-10-30 00:43:03 +0530

    digital clock implementation

--- JavaScript_Practice/2-BMICalculator/bmiCalculator.js
+++ JavaScript_Practice/2-BMICalculator/bmiCalculator.js
@@ -1,7 +1,16 @@
 const bmiCalulator = (height, weight) => {
   const result = document.querySelector("#results");
   const bmi = (weight / (height / 100) ** 2).toFixed(2);
-  result.innerHTML = bmi;
+
+  if (bmi <= 18.6) {
+    result.innerHTML = `${bmi} <span>Under Weight</span>`;
+  }
+  if (18.6 < bmi <= 24.9) {
+    result.innerHTML = `${bmi} <span>Normal</span>`;
+  }
+  if (24.9 < bmi) {
+    result.innerHTML = `${bmi} <span>Over Weight</span>`;
+  }
 };
 
 document.querySelector("button").addEventListener("click", (e) => {
